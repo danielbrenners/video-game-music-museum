65c642c2862db4eaf0a7adc4a286d891de327489
Daniel Brenners <danielbrenners@gmail.com>
2022-03-31 14:39:05 -0400

    edit snes order"

--- pages/snes/index.js
+++ pages/snes/index.js
@@ -61,18 +61,18 @@ export default function SNES() {
             description="The first video game with music. Although not during gameplay, this represented the first time music was used to create an atmosphere in a game."
           />
 
+          <MediaCard size="twoThirds" subtitle1="Figure 1. Hello world" />
+          <QuoteCard
+            quote="The ultimate goal for me in making music, or at least one of the main goals for me, is to create memorable melodies. That goal is there regardless of the tools we have."
+            author="Koji Kondo"
+          />
+
           <LabelCard
             title="Mega Man X"
             subtitle="Capcom, 1993"
             description="The first game to have continuous music, with sound effects overlaid during game play. The increase in tempo of the iconic four tones was due to a programming bug, but was kept as it creates a feeling of urgency for the player."
           />
           <MediaCard size="half" subtitle1="Figure 1. Hello world" />
-
-          <MediaCard size="twoThirds" subtitle1="Figure 1. Hello world" />
-          <QuoteCard
-            quote="The ultimate goal for me in making music, or at least one of the main goals for me, is to create memorable melodies. That goal is there regardless of the tools we have."
-            author="Koji Kondo"
-          />
 
           <MediaCard size="full" subtitle1="Figure 1. Hello world" />
         </Grid>
