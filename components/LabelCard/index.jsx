1b8ddc14d0bb5e8864a3544f02344a3a86073dc1
Daniel Brenners <danielbrenners@gmail.com>
2022-04-03 03:38:51 -0400

    custom audio button

--- components/LabelCard/index.jsx
+++ components/LabelCard/index.jsx
@@ -1,18 +1,20 @@
 import styles from "./styles.module.css";
+import useAudio from "../../hooks/useAudio";
 
 export default function LabelCard({ title, subtitle, description, audioSrc }) {
+  const [playing, toggle] = useAudio(audioSrc);
+
   return (
     <div className={styles.root}>
       <div className={styles.left}>
         <h2>{title}</h2>
         <h2>{subtitle}</h2>
+        <div onClick={toggle} className={styles.audioButton}>
+          <img src={playing ? "/images/pause.svg" : "/images/play.svg"} />
+        </div>
       </div>
       <div className={styles.right}>
         <p>{description}</p>
-        <audio controls>
-          <source src={audioSrc} type="audio/mpeg" />
-          Your browser does not support the audio element.
-        </audio>
       </div>
     </div>
   );
